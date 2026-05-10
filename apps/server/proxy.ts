1444335a4f1a95d0c9b0aa1c12f8d3d45d10eb29
njzydark <njzydark@gmail.com>
2026-05-11 00:18:04 +0800

    perf(server): imp proxy perf

--- apps/server/proxy.ts
+++ apps/server/proxy.ts
@@ -6,24 +6,24 @@ export const generateProxyRequest = async (c: Context, targetBaseUrl: string) =>
   const targetBase = new URL(targetBaseUrl);
   const targetUrl = `${targetBase.origin}${url.pathname}${url.search}`;
 
-  const clonedRequest = c.req.raw.clone();
-
   const newHeaders = new Headers(c.req.raw.headers);
   newHeaders.set("Host", targetBase.host);
 
-  let body: BodyInit | undefined = undefined;
-  const contentLength = parseInt(newHeaders.get("content-length") || "0");
-
-  if (contentLength > 0 || clonedRequest.body !== null) {
-    body = await clonedRequest.arrayBuffer();
+  if (c.req.raw.body !== null) {
+    const body = await c.req.raw.arrayBuffer();
     newHeaders.set("Content-Length", body.byteLength.toString());
+
+    return proxy(targetUrl, {
+      raw: c.req.raw,
+      headers: newHeaders,
+      body,
+      strictConnectionProcessing: true,
+    });
   }
 
   return proxy(targetUrl, {
-    ...c.req,
+    raw: c.req.raw,
     headers: newHeaders,
-    // @ts-ignore -- Hono's proxy typing issue
-    body,
     strictConnectionProcessing: true,
   });
 };
